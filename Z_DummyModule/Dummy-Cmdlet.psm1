b7a4a09c64aaad12dee5a4223dd2fa716adc6152
plodah <mleiper89@gmail.com>
2025-08-01 01:24:32 +0100

    Begin with dummy outline module

---
+++ Z_DummyModule/Dummy-Cmdlet.psm1
@@ -0,0 +1,30 @@
+﻿function Get-DummyThing {
+    [CmdletBinding()] 
+    param(
+        [Parameter( )]
+        [ValidateSet("abc","def")]
+        [string] $Name,
+    
+        [Parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [Alias("Server")] 
+        [string[]]$ComputerName = 'localhost',
+        [Parameter( )]
+        [PSCredential]  $Credential
+    )
+    begin{
+        $OutputType = "PlodahPS.Dummy"
+        Write-Warning "I haven't written this cmdlet yet."
+    }
+    process{
+        [PSCustomObject]$OutputObj=@{
+            Name="Hello"
+        }
+        $OutputObj.PsObject.TypeNames.Insert(0, $OutputType)
+        $OutputObj
+    }
+    end{
+    }
+}
---
+++ Z_DummyModule/Source/Create-DummyModule.ps1
@@ -0,0 +1,43 @@
+﻿########################################################
+##            Something about this module             ##
+##                                                    ##
+########################################################
+## Basic Info;
+##    Create a new GUID per module, and give it a description.
+$Splattable = @{
+    Guid = "00000000-0000-0000-0000-000000000000"
+    Author = "plodah"
+    CompanyName = "none"
+    Description = "This is a dummy description"
+}
+#Paths. No Change Needed
+$RootDir = (Get-Item $PSScriptRoot).Parent
+$Splattable.Path = "$($RootDir.FullName)\$($RootDir.Name).psd1"
+#ModuleFiles- Enter the filenames to be included. 
+$Files = @(
+    "Dummy-Cmdlet.psm1"
+)
+$Splattable.RootModule = ($Files | Select-Object -First 1) 
+$Splattable.NestedModules = @( $Files | Select-Object -Skip 1 )
+#Cmdlets
+$Splattable.CmdletsToExport = @(
+    "Get-DummyThing"
+)
+$Splattable.FunctionsToExport = $Splattable.CmdletsToExport
+#Aliases
+$Splattable.AliasesToExport = @(
+    
+)
+#Formats
+$Splattable.FormatsToProcess = @(
+    "Formats\Dummy.Format.ps1xml"
+)
+#Pre-resquisite modules
+$Splattable.RequiredModules = @(
+    
+)
+#ObjectTypes
+$Splattable.TypesToProcess = @(
+)
+#CREATION
+New-ModuleManifest @Splattable
